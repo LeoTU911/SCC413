4dbd872f5f0917b30e175f090d4a97e0636993f7
LeoTU911 <122132815+LeoTU911@users.noreply.github.com>
2023-02-28 21:11:11 +0000

    Update test.py

--- pytorch-CycleGAN/test.py
+++ pytorch-CycleGAN/test.py
@@ -27,6 +27,7 @@ from data import create_dataset
 from models import create_model
 from util.visualizer import save_images
 from util import html
+import joblib
 
 try:
     import wandb
@@ -72,3 +73,5 @@ if __name__ == '__main__':
             print('processing (%04d)-th image... %s' % (i, img_path))
         save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize, use_wandb=opt.use_wandb)
     webpage.save()  # save the HTML
+    
+    joblib.dump(model, './results/cycleGAN.pkl')
